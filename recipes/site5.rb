cabd5d409cc7cd658fc2ce07be9cb4f020a9217d
Daniel Quimper <dquimper@gmail.com>
2011-03-30 20:04:58 -0400

    default site5 dir

--- recipes/site5.rb
+++ recipes/site5.rb
@@ -1,7 +1,7 @@
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set(:deploy_to) { "/home/#{user}/#{application}.dansrc.com/#{application}" }
+set(:deploy_to) { "/home/#{user}/rails/#{application}" }
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
