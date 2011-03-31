68276b350e5f1ca11ff05bbe448e18412697979b
Daniel Quimper <dquimper@gmail.com>
2011-03-31 13:44:23 -0400

    removing useless comments

--- recipes/site5.rb
+++ recipes/site5.rb
@@ -1,6 +1,3 @@
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set(:deploy_to) { "/home/#{user}/rails/#{application}" }
 set(:public_html) { "/home/#{user}/public_html/#{application}"}
 
